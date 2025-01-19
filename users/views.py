859ce9a35aa0bb1bd00dfb3389a67b22a9db3fd9
OmShirke <113794365+OmShirke@users.noreply.github.com>
2025-01-19 13:38:21 +0530

    idk

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     if request.method == 'POST':
@@ -11,3 +13,19 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
+def user_home(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Replace 'home' with your desired route name
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
